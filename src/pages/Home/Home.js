b9c34b8911460299d2f52d378e5441997bca6e28
Muminur Rahman <r.muminur@gmail.com>
2021-08-03 00:05:21 +0600

    Fix

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import logo from "../../logo.svg";
-import "./Home.css";
 import Topbar from "../../components/Topbar";
 import {Container} from "reactstrap";
 
 const Home = () => {
     return (
         <React.Fragment>
-            <Container>
+            <Container fluid className="vh-100">
                 <Topbar/>
-                <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo"/>
-                        <p>
-                            Edit <code>src/App.js</code> and save to reload.
-                        </p>
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Learn React
-                        </a>
-                    </header>
+                <div className="text-center">
+                    <p className="text-capitalize">Hey, I am</p>
+                    <h1 className="text-capitalize">Muminur Rahman</h1>
                 </div>
             </Container>
         </React.Fragment>
